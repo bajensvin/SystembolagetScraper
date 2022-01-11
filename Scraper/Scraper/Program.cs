b19db360b30c64f83d83e79a834c43ba0057644e
Micke <beemr1@gmail.com>
2022-01-11 16:08:12 +0100

    added some stuff for scraping

---
+++ Scraper/Scraper/Program.cs
@@ -0,0 +1,43 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Scraper
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var setup = new WebDriverSetup();
+            var driver = setup.SetupDriver();
+            driver.Navigate().GoToUrl($"{driver.Url}/nytt");
+            var doc = new HtmlDocument();
+            doc.LoadHtml(driver.PageSource);
+
+            var upcomingReleasesContainer = doc.DocumentNode.SelectNodes("/html/body/div[1]/div[2]/main/div[2]/div/div/div[1]/div/div[1]/div/div/div");
+
+            var datePattern = new Regex(@"\d+\/\d+");
+            var wordPattern = new Regex(@"[a-zA-ZåäöÅÄÖ ]");
+            var upcomingReleasesNodes = upcomingReleasesContainer.Select(x => x.SelectNodes(".//a[contains(@href, 'sok')]")).First();
+            
+            var upcomingLaunches = upcomingReleasesNodes.Select(x => new UpcomingLaunch { Date = datePattern.Match(x.InnerText).Value, Link = new Uri(HttpUtility.HtmlDecode(x.GetAttributeValue("href", string.Empty))), Type = HttpUtility.HtmlDecode(x.InnerText.Substring(x.InnerText.IndexOf(' ') + 1)) }).ToList();
+
+            //foreach (var link in upcomingLaunches.Select(y => y.Link))
+            //{
+            //    driver.Navigate().GoToUrl(new Uri(""));
+            //}
+
+            driver.Quit();
+        }
+    }
+
+    class UpcomingLaunch
+    {
+        public string Type { get; set; }
+        public string Date { get; set; }
+        public Uri Link { get; set; }
+    }
+
+}
---
+++ Scraper/Scraper/WebDriverSetup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scraper
+{
+    public class WebDriverSetup
+    {
+        private readonly string _url;
+        private readonly ChromeDriver _driver;
+        public WebDriverSetup()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _url = config["Urls:MainSite"];
+            _driver = new ChromeDriver
+            {
+                Url = _url
+            };
+        }
+        public ChromeDriver SetupDriver()
+        {
+            var cookies = new List<Cookie>
+            {
+                new Cookie("isAgeVerified", "1", "/"),
+                new Cookie("cookieConsent", "[]", "/"),
+                new Cookie("ai_user", "nl+1f|2022-01-11T10:28:55.407Z", "/"),
+                new Cookie("ai_session", "4Act/|1641896935493|1641898794861", "/")
+            };
+
+            foreach(var cookie in cookies)
+            {
+                _driver.Manage().Cookies.AddCookie(cookie);
+            }
+
+            _driver.Manage().Window.Maximize();
+            return _driver;
+        }
+    }
+}
