a161061a97b16b302e0d4501ff7e93695a13fe64
Michael Lindfors <mlindfors@rcanalytics.com>
2022-01-12 12:54:39 +0100

    added collection of beer info from result list

--- Scraper/Scraper/Program.cs
+++ Scraper/Scraper/Program.cs
@@ -1,5 +1,9 @@
 ﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
@@ -22,12 +26,53 @@ namespace Scraper
             var wordPattern = new Regex(@"[a-zA-ZåäöÅÄÖ ]");
             var upcomingReleasesNodes = upcomingReleasesContainer.Select(x => x.SelectNodes(".//a[contains(@href, 'sok')]")).First();
             
-            var upcomingLaunches = upcomingReleasesNodes.Select(x => new UpcomingLaunch { Date = datePattern.Match(x.InnerText).Value, Link = new Uri(HttpUtility.HtmlDecode(x.GetAttributeValue("href", string.Empty))), Type = HttpUtility.HtmlDecode(x.InnerText.Substring(x.InnerText.IndexOf(' ') + 1)) }).ToList();
+            var upcomingLaunches = upcomingReleasesNodes.Select(x => new UpcomingLaunch { Date = DateTime.Parse(datePattern.Match(x.InnerText).Value, CultureInfo.CurrentCulture), Link = new Uri(HttpUtility.HtmlDecode(x.GetAttributeValue("href", string.Empty))), Type = HttpUtility.HtmlDecode(x.InnerText.Substring(x.InnerText.IndexOf(' ') + 1)) }).ToList();
 
-            //foreach (var link in upcomingLaunches.Select(y => y.Link))
-            //{
-            //    driver.Navigate().GoToUrl(new Uri(""));
-            //}
+            foreach (var launch in upcomingLaunches)
+            {
+                driver.Navigate().GoToUrl(launch.Link);
+
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]")));
+
+                var showMoreButtonSelector = By.XPath("//button[text()='Visa fler ']");
+
+                if (driver.FindElements(showMoreButtonSelector).Count > 0)
+                {
+                    var showMoreButton = driver.FindElement(showMoreButtonSelector);
+
+                    while (showMoreButton.Displayed)
+                    {
+                        try
+                        {
+                            showMoreButton.Click();
+                            showMoreButton = driver.FindElement(showMoreButtonSelector);
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                var beerListDoc = new HtmlDocument();
+                beerListDoc.LoadHtml(driver.PageSource);
+                var beerlist = beerListDoc.DocumentNode.SelectNodes("/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div[4]");
+                var beers = beerlist.Select(x => x.SelectNodes(".//a[contains(@href, 'produkt/ol')]")).First();
+
+                foreach (var beer in beers)
+                {
+                    var bla = new Beer
+                    {
+                        Name = beer.SelectSingleNode("//div/div/div[3]/div/div/div/div/h3").InnerText,
+                        Type = beer.SelectSingleNode("//div/div/div[3]/div/div/div/h4").InnerText,
+                        Description = beer.SelectSingleNode("//div/div/div[3]/div/div/div[2]/div[1]").InnerText,
+                        Country = beer.SelectSingleNode("//div/div/div[3]/div/div/div/div[1]/span[2]").InnerText,
+                        Size = beer.SelectSingleNode("//div/div/div[3]/div[2]/div[1]/div[2]").InnerText,
+                        //Price = Convert.ToDecimal(beer.SelectSingleNode("//div/div/div[3]/div[2]/div[3]/span[1]"), CultureInfo.CurrentCulture)
+                    };
+                }
+            }
 
             driver.Quit();
         }
@@ -36,8 +81,18 @@ namespace Scraper
     class UpcomingLaunch
     {
         public string Type { get; set; }
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public Uri Link { get; set; }
+    }
+
+    class Beer
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public decimal Price { get; set; }
+        public string Size { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
     }
 
 }
